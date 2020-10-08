7a506978949cdf178d272264e4697e42c591cb04
Iris <royalvald@sina.com>
2020-10-08 20:30:55 +0800

    add turbine component

--- SpringCloudDemo/turble/src/main/java/com/iris/demo/TurbineApplication.java
+++ SpringCloudDemo/turble/src/main/java/com/iris/demo/TurbineApplication.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableEurekaClient
-public class FeignApplication {
+@EnableTurbine
+public class TurbineApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FeignApplication.class,args);
+        SpringApplication.run(TurbineApplication.class,args);
     }
 }
