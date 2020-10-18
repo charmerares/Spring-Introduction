14a880c791b3283b55eb966eae7dceff6041e6f1
Iris <royalvald@sina.com>
2020-10-18 19:59:09 +0800

    add sentinel and nacos

---
+++ sentinelNew/src/main/java/com/iris/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.iris.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String test(){
+        return "Hello SpringBoot sentinel";
+    }
+}
